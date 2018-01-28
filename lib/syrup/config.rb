8322fc46fbe962417454fd1284c3a7b2e092b391
Marcelo Gheiler <marcelogheiler@gmail.com>
2018-01-28 04:18:54 -0500

    Add non-multiline rule support

--- lib/syrup/config.rb
+++ lib/syrup/config.rb
@@ -18,109 +18,21 @@ module Syrup
 
             replace_map = Hash.new
             
-            home_config["rules"].each do |ruleObj| 
 
-                split_rules = [[], []] # is size 2
-                ruleObj["rule"].inject(0) do |acc, e|
-                    if '-->>' == e
-                        acc += 1
-                    else
-                        split_rules[acc].append(e)
-                        acc
-                    end
-                end
+            # home_config["rules"].each do |ruleObj| 
+                # processRules(replace_map, ruleObj)
+                # break
+            # end
 
-                # Combine multiple lines into one
-                source = split_rules[0].inject { |acc, var| acc + "\n" + var }
-                target = split_rules[1].inject { |acc, var| acc + "\n" + var }
+            local_config["rules"].each do |ruleObj|
 
-                source = source.split(/(\$\$\$[^\$\$\$]*\$\$\$)/).reject { |source| source.empty? }
-                target = target.split(/(\$\$\$[^\$\$\$]*\$\$\$)/).reject { |source| source.empty? }
-
-                # Boolean, true if it is multiline, false otherwise
-                multiline = ruleObj["multiline"] == true ? true : false
-
-                if (multiline) then
-
-                    # Sanity checks
-                    # First and last sections /MUST/ be symbols
-                    raise "Multiline rules can not start or end with a variable" if (isVariable(source[0]) || isVariable(source[-1]))
-
-                    # Ensure that every other section is a variable
-                    source.each_with_index do |e, i|
-                        if (i % 2 == 0) then
-                            if (isVariable(source[i])) then
-                                raise "Delimiter splitting failed"
-                            end
-                        else
-                            if (!isVariable(source[i])) then
-                                raise "Delimiter splitting failed"
-                            end
-                        end
-                    end
-
-                    regx = "(?<___SYPCOMPLETE___>"
-
-                    # Add regex boilerplate for the first three sections
-                    fs = Regexp.escape(source[0]) # First symbol
-                    fv = variableName(source[1]) # First variable
-                    ss = Regexp.escape(source[2]) # Second symbol
-
-                    regx += fs
-                    regx += "(?<#{fv}>"
-                    regx += "[^#{ss}]*)"
-                    regx += ss
-
-                    # If there are more than three sections, add them iteratively here
-                    i = 2
-                    while (i < source.size - 1) do
-                        nv = variableName(source[i + 1]) # Next variable
-                        ns = Regexp.escape(source[i + 2]) # Next symbol
-
-                        regx += "(?<#{nv}>"
-                        regx += "[^#{ns}]*)"
-                        regx += ns
-                        i += 3
-                    end
-
-                    regx += ")"
-                    regx = Regexp.new(regx, Regexp::MULTILINE)
-                    p regx
-                end
+                processRules(replace_map, ruleObj)
                 break
-                pp "=========="
-
-            elsif (!multiline) then
-                
             end
-                
 
             # replace_map = {
                 # /(?<complete>if\ \[(?<condition>[^\]]+)\])/m => "if (%{condition})",
             # }
-
-            # # Merge local and home configs
-            # # local config has higher priority than home config
-            # if (!home_config.nil? && !local_config.nil?) then
-
-            #     # This combines local and home config, and removes duplicates
-            #     rules = local_config["rules"] + home_config["rules"]
-
-            #     # Take all info from the local config, but with the combined rules
-            #     config = local_config
-            #     config["rules"] = rules.uniq
-
-            #     pp config
-            #     return config
-            # end
-
-            # # This is the case where only one or the other exists
-            # # so no merging is needed
-            # if (!home_config.nil? && local_config.nil?) then
-            #     return home_config
-            # else
-            #     return local_config
-            # end
         end
 
         def self.isVariable(str)
@@ -131,11 +43,119 @@ module Syrup
             return str.match(/\$\$\$([^\$\$\$]+)\$\$\$/)[1]
         end
 
+        def self.processRules(replace_map, ruleObj)
+
+            split_rules = [[], []] # is size 2
+            ruleObj["rule"].inject(0) do |acc, e|
+                if '-->>' == e
+                    acc += 1
+                else
+                    split_rules[acc].append(e)
+                    acc
+                end
+            end
+
+            # Combine multiple lines into one
+            source = split_rules[0].inject { |acc, var| acc + "\n" + var }
+            target = split_rules[1].inject { |acc, var| acc + "\n" + var }
+
+            source = source.split(/(\$\$\$[^\$\$\$]*\$\$\$)/).reject { |source| source.empty? }
+            target = target.split(/(\$\$\$[^\$\$\$]*\$\$\$)/).reject { |source| source.empty? }
+
+            # Boolean, true if it is multiline, false otherwise
+            multiline = ruleObj["multiline"] == true ? true : false
+
+            validateRule(source)
+
+            if (multiline) then
+
+                # Sanity checks
+                # First and last sections /MUST/ be symbols
+                raise "Multiline rules can not start or end with a variable" if (isVariable(source[0]) || isVariable(source[-1]))
+
+                # Ensure that every other section is a variable
+                source.each_with_index do |e, i|
+                    if (i % 2 == 0) then
+                        if (isVariable(source[i])) then
+                            raise "Delimiter splitting failed"
+                        end
+                    else
+                        if (!isVariable(source[i])) then
+                            raise "Delimiter splitting failed"
+                        end
+                    end
+                end
+
+                regx = "(?<___SYPCOMPLETE___>"
+
+                # Add regex boilerplate for the first three sections
+                fs = Regexp.escape(source[0])   # First symbol
+                fv = variableName(source[1])    # First variable
+                ss = Regexp.escape(source[2])   # Second symbol
+
+                regx += fs
+                regx += "(?<#{fv}>"
+                regx += "[^#{ss}]*)"
+                regx += ss
+
+                # If there are more than three sections, add them iteratively here
+                i = 2
+                while (i < source.size - 1) do
+                    nv = variableName(source[i + 1])    # Next variable
+                    ns = Regexp.escape(source[i + 2])   # Next symbol
+
+                    regx += "(?<#{nv}>"
+                    regx += "[^#{ns}]*)"
+                    regx += ns
+                    i += 3
+                end
+
+                regx += ")"
+                regx = Regexp.new(regx, Regexp::MULTILINE)
+
+                rhs = ""
+                target.each do |section| 
+                    if (isVariable(section)) then
+                        rhs += "%{#{variableName(section)}}"
+                    else
+                        rhs += section
+                    end
+                end
+
+                replace_map[regx] = rhs
+            elsif (!multiline)
+                regx = "(?<___SYPCOMPLETE___>"
+                source.each_with_index do |section, index|
+                    if (isVariable(section)) then
+                        if (index == source.size - 1) then
+                            regx += "(?<#{variableName(section)}>"
+                            regx += ".*"
+                        else
+                            regx += "(?<#{variableName(section)}>"
+                        end
+                    else
+                        if (index == 0) then
+                            regx += Regexp.escape(section)
+                        else
+                            regx += "[^#{Regexp.escape(section)}]*)"
+                            regx += Regexp.escape(section)
+                        end
+                    end
+                end
+                regx += ")"
+                p regx
+            end
+        end
+        
         def self.read(filetype)
-            # Unprocessed config
-            config = find(filetype)
+        # Unprocessed config
+        config = find(filetype)
+        end
 
+        def self.validateRule(rule)
+            # Check if rule is length one and only a variable
 
+            return true
         end
 
         def self.verify
